feff7d8f7b871dcb9decc040e81f0637f8dec0ce
V1nnyyy05 <ervinhienz@gmail.com>
2025-09-15 22:02:55 +0800

    ADD SW1 folder

---
+++ SW1/Seatwork1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seatwork1
+{
+    class Program
+    {
+        static bool IsPrime(int num)
+        {
+            if (num <= 1) return false; 
+
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                    return false; 
+            }
+            return true; 
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter string: ");
+            string input = Console.ReadLine();
+
+            string[] numbers = input.Split(' ');
+
+            int count = 1;
+            foreach (string str in numbers)
+            {
+                if (int.TryParse(str, out int num))
+                {
+                    if (IsPrime(num))
+                        Console.WriteLine($"{count}.  {num}\tPrime");
+                    else
+                        Console.WriteLine($"{count}.  {num}\tComposite");
+                    count++;
+                }
+            }Console.ReadKey();
+        }
+        
+    }
+}
---
+++ SW1/Seatwork2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seatwork2
+{
+    class Program
+    {
+        const double DollarToPeso = 57.17;
+        const double DollarToYen = 146.67;
+        static (double peso, double yen) ConvertCurrency(double dollars)
+        {
+            double peso = dollars * DollarToPeso;
+            double yen = dollars * DollarToYen;
+            return (peso, yen);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter currency in ($): ");
+            string input = Console.ReadLine();
+
+            string[] amounts = input.Split(' ');
+
+            Console.WriteLine("\nDollar($)\tPhil Peso(P)\tJpn Yen(¥)");
+
+            foreach (string str in amounts)
+            {
+                if (double.TryParse(str.Trim(), out double dollars))
+                {
+                    var (peso, yen) = ConvertCurrency(dollars);
+                    Console.WriteLine($"{dollars}\t\t{peso:F2}\t\t{yen:F2}");
+                }
+            }
+
+           Console.ReadKey();
+        } 
+    }
+}
